67270e55fb7152219822cf7cb13560e3db100efc
GarshaBabadi <garshababadireyhani@gmail.com>
2025-11-12 21:19:03 +0330

    Set IRepository interfaces

--- DevSpot/Repositories/IRepository.cs
+++ DevSpot/Repositories/IRepository.cs
@@ -1,6 +1,11 @@
 ﻿namespace DevSpot.Repositories
 {
-    public interface IRepository<T>
+    public interface IRepository<T> where T : class
     {
+        Task<IEnumerable<T>> GetAllAsync();
+        Task<T> GetByIdAsync(int id);
+        Task UpdateAsync(T entity);
+        Task AddAsync(T entity);
+        Task DeleteAsync(int id);
     }
 }
