0a5f934027509ec7955cde865d787a4aa6b0a350
GarshaBabadi <garshababadireyhani@gmail.com>
2025-11-18 19:57:43 +0330

    creating the form for job postings

--- DevSpot/Controllers/JobPostingsController.cs
+++ DevSpot/Controllers/JobPostingsController.cs
@@ -25,5 +25,11 @@ namespace DevSpot.Controllers
         {
             return View();
         }
+
+        [HttpPost]
+        public async Task<IActionResult> Create(JobPosting jobPosting)
+        {
+            return RedirectToAction(nameof(Index));
+        }
     }
 }
